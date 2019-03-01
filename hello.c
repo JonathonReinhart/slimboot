932d8a09f7f0c46ca36a17098c943334c26db90d
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2019-03-01 08:48:04 -0500

    Reboot after booting

--- hello.c
+++ hello.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/reboot.h>
 
 
 int main(int argc, char *argv[])
 {
+    int rc;
+
     printf("HELLO WORLD!\n");
 
     printf("argv:\n");
-    int i;
-    for (i = 0; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i) {
         printf("[%d] = \"%s\"\n", i, argv[i]);
     }
 
+    sync();
+    rc = reboot(RB_POWER_OFF);
+    if (rc) {
+        printf("reboot() returned %d\n", rc);
+    }
+
     pause();
+
     return 0; 
 }
