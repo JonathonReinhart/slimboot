2adc4aa52bbeb6725c0eec29f4faec1a7fa9dc14
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2019-03-01 08:49:40 -0500

    Remove pidfile when done

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import os
 import sys
 import platform
 from subprocess import Popen
 
+pidfile = "qemu.pid"
 kernel_bzimage = "/boot/vmlinuz-" + platform.release()
 kernel_initrd = "initrd-1.img"
 kernel_cmdline = ' '.join((
@@ -19,9 +21,11 @@ qemu_opts = [
     '-kernel', kernel_bzimage,
     '-initrd', kernel_initrd,
     '-append', kernel_cmdline,
-    '-pidfile', 'qemu.pid',
+    '-pidfile', pidfile,
 ]
 
 
 p = Popen(['qemu-kvm'] + qemu_opts)
 p.wait()
+
+os.remove(pidfile)
