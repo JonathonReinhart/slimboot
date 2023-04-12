c8800ab87a8ce375773dfbae19657b8a4b0a8bac
Jonathon Reinhart <jrreinhart@google.com>
2023-04-12 13:19:36 -0400

    Ensure files in initrd are owned by root

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 mkdir -p initrd
 gcc -o initrd/init -Wall -Werror -static hello.c
 
-(cd initrd && find . | cpio -o -H newc | gzip > ../initrd-1.img)
+(cd initrd && find . | cpio -o -H newc -R '+0:+0' | gzip > ../initrd-1.img)
