3cfaafb828442f39984ef0d45df56759fe8759c4
Jonathon Reinhart <jrreinhart@google.com>
2023-04-12 13:25:47 -0400

    Clean up build.sh a bit

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-mkdir -p initrd
-gcc -o initrd/init -Wall -Werror -static hello.c
+WORKDIR="initrd"
+IMAGE="initrd-1.img"
 
-(cd initrd && find . | cpio -o -H newc -R '+0:+0' | gzip > ../initrd-1.img)
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+
+# Build sample app
+gcc -o $WORKDIR/hello -Wall -Werror -static hello.c
+(
+    cd $WORKDIR
+    ln -s hello init
+)
+
+# Generate initrd
+(cd $WORKDIR && find . | cpio -o -H newc -R '+0:+0') | gzip > $IMAGE
